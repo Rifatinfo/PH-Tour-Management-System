cfbbe7511d51cd7859f9e7f182d55762b6431c06
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-07-12 21:10:48 +0600

    Part-1: Implemented user valiadtion

---
+++ src/app/modules/user/user.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { UserControllers } from "./user.controller";
+import { createUserZodSchema } from "./user.validation";
+import { validateRequest } from "../middlwares/validRequest";
+
+const router = Router()
+
+router.post("/register", 
+    validateRequest(createUserZodSchema)
+, UserControllers.createUser);
+   
+router.get("/all-users", UserControllers.getAllUsers);
+
+export const UserRoutes = router;
