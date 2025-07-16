c9935b21989d43688204a7a6fc91d3dd9f3fc67a
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-07-16 06:13:01 +0600

    add logout fuunction

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post("/login", AuthController.credentialLogin);
 router.post("/refresh-token", AuthController.getNewAccessToken);
+router.post("/logout", AuthController.logout);
 
 export const AuthRoutes = router;
